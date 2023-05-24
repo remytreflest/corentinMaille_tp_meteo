e29ea28151de305b360815fcbc8eb56b5b0c03d1
treflest remy <mrtreflestremy@outlook.com>
2023-05-24 14:34:33 +0200

    save

---
+++ src/components/Resultats.js
@@ -0,0 +1,13 @@
+const Resultats = ({ title, experience, date }) => {
+    return (
+        <div className='card text-center m-3'>
+            <div className='card-header card-header-subsection'>{title}</div>
+            <div className='card-body'>
+                <p className='card-text'>{experience}</p>
+            </div>
+            <div className='card-footer text-muted'>{date}</div>
+        </div>
+    )
+}
+
+export default Resultats
